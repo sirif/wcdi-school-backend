e36a91cc73c4bb14dfbf59ab34b7d970be44f68c
saya <a.saiko@asaikohp.uni.innopolis.ru>
2023-10-17 13:03:51 +0300

    Парсинг xlsx для алгебры

--- home_work_checker/algebra/a_1.py
+++ home_work_checker/algebra/a_1.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from os import path as os_path
 from home_work_checker.base_estimator import BaseEstimator
 
@@ -9,8 +10,30 @@ class A1(BaseEstimator):
         self.__work_number__: int = 1
 
     def check(self, file_name: os_path) -> int:
-        # file_name = BaseEstimator.fill_filename(work_dir=self.__work_dir__, work_uuid=work_uuid)
-        return 4
+        answers = [10, 12, 11, 3, 4]
+        errors = 0
+        content = pd.read_excel(file_name, skiprows=[0])
+        results = content.values[:, 4]
+        for ans, res in zip(answers, results):
+            try:
+                if ans == int(res):
+                    continue
+                else:
+                    errors += 1
+            except Exception as e:
+                print(str(e))
+                errors += 1
+        if errors == 0:
+            return 5
+        elif errors == 1:
+            return 4
+        elif errors == 2:
+            return 3
+        elif errors in [3, 4]:
+            return 2
+        else:
+            return 1
+        # return 0
     
 
 # 1+2=3
