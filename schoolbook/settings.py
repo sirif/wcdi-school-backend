9b76819a66ac9ca0f3a2b811852f03f98882dfb9
angie <angie@example.com>
2023-10-26 09:46:58 +0500

    Tests for teacher

--- schoolbook/settings.py
+++ schoolbook/settings.py
@@ -101,6 +101,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': 'test_database',
+        },
     }
 }
 
